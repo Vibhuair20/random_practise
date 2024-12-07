1a9cddba3323a570ca9243d322b9a6dc7d6d735f
Vibhuair20 <114051581+Vibhuair20@users.noreply.github.com>
2024-12-07 17:19:17 +0530

    recursion-1.1

---
+++ selection_sort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void selection(int arr[], int n){
+    //base case
+    
+    //return
+    //recursive call
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+
+    selection(arr,n);
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i]>> " ";
+    }cout<<end;
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "<<endl;
+    }
+}
