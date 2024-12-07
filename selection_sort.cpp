79e245ec23aeafa2d5037776b2c78d6a779e4f1f
Vibhuair20 <114051581+Vibhuair20@users.noreply.github.com>
2024-12-07 17:39:30 +0530

    selection

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,11 +1,23 @@
 #include<iostream>
 using namespace std;
 
-void selection(int arr[], int n){
+void selection(int arr[], int n,int index=0){
     //base case
-    
+    if(index==n-1){
+        return;
+    }
     //return
     //recursive call
+    int minindex= index;
+    for(int i=index+1;i<n;i++){
+        if(arr[i]<arr[minindex]){
+            minindex=i;
+        }
+    }
+    swap(arr[index], arr[minindex]);
+    cout<<"the steps are: "<<arr<<endl;
+    selection(arr,n,index+1);
+
 }
 
 int main(){
@@ -13,12 +25,12 @@ int main(){
     cin>>n;
     int arr[n];
 
-    selection(arr,n);
 
     for(int i=0;i<n;i++){
-        cin>>arr[i]>> " ";
-    }cout<<end;
+        cin>>arr[i];
+    }cout<<endl;
 
+    selection(arr,n);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" "<<endl;
     }
